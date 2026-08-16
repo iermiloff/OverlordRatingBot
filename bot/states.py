4ea50b648a37de69d500e4252077e9ac84eaeac7
iermiloff <109422436+iermiloff@users.noreply.github.com>
2026-08-16 22:08:38 +0300

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -2,3 +2,6 @@ from aiogram.fsm.state import StatesGroup, State
 
 class OrderCheckout(StatesGroup):
     waiting_for_delivery_data = State() # Ожидание ФИО и адреса доставки
+
+class ManagerUserActions(StatesGroup):
+    waiting_for_rating_amount = State()  # Ожидание ввода числа для изменения баланса
