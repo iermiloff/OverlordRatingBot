afbd916d53f8c19136c1cf4293579fff0e8fa180
iermiloff <109422436+iermiloff@users.noreply.github.com>
2026-08-16 22:18:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from bot.handlers.user_tasks import router as user_tasks_router
 from bot.handlers.manager_users import router as manager_users_router
 from bot.handlers.manager_shop import router as manager_shop_router
 from bot.handlers.manager_orders import router as manager_orders_router
+from bot.handlers.manager_antifraud import router as manager_antifraud_router
 
 
 # Настраиваем логирование, чтобы видеть состояние бота в консоли
@@ -62,6 +63,7 @@ async def main():
     dp.include_router(manager_users_router)
     dp.include_router(manager_shop_router)
     dp.include_router(manager_orders_router)
+    dp.include_router(manager_antifraud_router)
     dp.include_router(chat_activity_router)
         # Запускаем бесконечный цикл обработки обновлений
         await dp.start_polling(bot)
