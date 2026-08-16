ed65c521fe2a8700e6b9c36eb6c371c63a06c035
iermiloff <109422436+iermiloff@users.noreply.github.com>
2026-08-17 00:27:10 +0300

    Update menu_kb.py

--- bot/keyboards/menu_kb.py
+++ bot/keyboards/menu_kb.py
@@ -2,14 +2,14 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import settings
 
 def get_user_inline_menu() -> InlineKeyboardMarkup:
-    """Генерирует экранное меню для обычного пользователя."""
+    """Генерирует главное экранное меню для обычного пользователя."""
     return InlineKeyboardMarkup(inline_keyboard=[
         [
             InlineKeyboardButton(text="📊 Моя статистика", callback_data="user_stats"),
             InlineKeyboardButton(text="🛍️ Магазин товаров", callback_data="shop_page:1")
         ],
         [
-       #     InlineKeyboardButton(text="🤝 Партнерка", callback_data="user_referrals"),
+        #    InlineKeyboardButton(text="🤝 Партнерка", callback_data="user_referrals"),
             InlineKeyboardButton(text="🎖️ Список титулов", callback_data="user_titles")
         ],
         [
@@ -22,7 +22,7 @@ def get_user_inline_menu() -> InlineKeyboardMarkup:
     ])
 
 def get_manager_inline_menu() -> InlineKeyboardMarkup:
-    """Генерирует экранное меню для менеджера."""
+    """Генерирует главное экранное меню для менеджера/администратора."""
     return InlineKeyboardMarkup(inline_keyboard=[
         [
             InlineKeyboardButton(text="👥 Список пользователей", callback_data="mg_users_page:1"),
@@ -41,7 +41,7 @@ def get_manager_inline_menu() -> InlineKeyboardMarkup:
     ])
 
 def get_back_to_menu_keyboard(to_manager: bool = False) -> InlineKeyboardMarkup:
-    """Универсальная кнопка возврата в ЛК для предотвращения тупиков."""
+    """Универсальная экранная кнопка возврата для предотвращения тупиков в интерфейсе."""
     target = "main_menu_manager" if to_manager else "main_menu_user"
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="↩️ Вернуться в главное меню", callback_data=target)]
