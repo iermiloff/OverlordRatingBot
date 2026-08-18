150ba3d8b1289704086d5c58daf337a45f3be1b0
iermiloff <109422436+iermiloff@users.noreply.github.com>
2026-08-19 01:39:33 +0300

    Update menu_kb.py

--- bot/keyboards/menu_kb.py
+++ bot/keyboards/menu_kb.py
@@ -4,19 +4,18 @@ def get_user_inline_menu() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(inline_keyboard=[
         [
             InlineKeyboardButton(text="📊 Моя статистика", callback_data="user_stats"),
-            InlineKeyboardButton(text="🛍️ Магазин товаров", callback_data="shop_page:1")
+            InlineKeyboardButton(text="🛍️ Магазин товаров", callback_data="user_shop_main")
         ],
         [
             InlineKeyboardButton(text="🤝 Партнерка", callback_data="user_referrals"),
             InlineKeyboardButton(text="🎖️ Список титулов", callback_data="user_titles")
         ],
         [
-            InlineKeyboardButton(text="📝 Задания", callback_data="user_tasks"),
+            InlineKeyboardButton(text="📝 Задания", callback_data="user_tasks_main"),
             InlineKeyboardButton(text="💬 Наши Чаты", callback_data="user_chats")
         ],
         [
-            InlineKeyboardButton(text="🎒 Мой Инвентарь", callback_data="user_inventory"), # СЮДА: Инвентарь
-            InlineKeyboardButton(text="🎁 Мои Награды", callback_data="rewards_page:1")
+            InlineKeyboardButton(text="🎒 Мой Инвентарь", callback_data="user_inventory_main")
         ]
     ])
 
